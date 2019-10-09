53f3ac86f11f3dda435936929090a0eadc9b12b5
imartinezl <inigomlap@gmail.com>
2019-10-09 15:17:45 +0200

    bigram to ngram + pair of words correlation

--- text_exploration.R
+++ text_exploration.R
@@ -22,7 +22,7 @@ cleanHTML <- function(htmlString) {
 mystopwords <- tibble(word = c("\n"))
 
 df_words <- df %>% 
-  dplyr::select(id, Job_Title, Job_Type, Description) %>% 
+  dplyr::select(id, Job_Type, Description) %>% 
   # dplyr::slice(1:20) %>%
   dplyr::mutate(Description = cleanHTML(Description)) %>% 
   tidytext::unnest_tokens(word, Description) %>% # tokenize
@@ -47,9 +47,7 @@ df_words %>%
   ggplot2::coord_flip()
 
 df_words %>%
-  dplyr::group_by(Job_Type) %>% 
-  dplyr::count(word, sort = TRUE) %>%
-  dplyr::ungroup() %>% 
+  dplyr::count(word, Job_Type, sort = TRUE) %>%
   dplyr::filter(n > 15) %>%
   dplyr::top_n(50, n) %>%
   dplyr::mutate(word = reorder(word, n)) %>%
@@ -58,7 +56,6 @@ df_words %>%
   ggplot2::facet_grid(cols=vars(Job_Type))+
   ggplot2::xlab(NULL) +
   ggplot2::coord_flip()
-
 
 # Sentiment Analysis ------------------------------------------------------
 
@@ -74,6 +71,7 @@ df_words %>%
   reshape2::acast(word ~ sentiment, value.var = "n", fill = 0) %>%
   wordcloud::comparison.cloud(colors = c("red4", "seagreen"), 
                               rot.per = 0, scale = c(5,1), max.words = 100)
+
 
 
 # tf-idf ------------------------------------------------------------------
@@ -120,42 +118,104 @@ df_words %>%
   ggplot2::coord_flip()
 
 
+
 # N-grams and correlations ------------------------------------------------
 
-mystopwords <- tibble(word = c("\n"))
+mystopwords <- tibble(word = c("\n", "xi"))
 stop_words <- dplyr::bind_rows(tidytext::stop_words, mystopwords)
 
-df_bigrams <- df %>% 
+ng <- 2
+df_ngrams <- df %>% 
   dplyr::select(id, Job_Title, Job_Type, Description) %>% 
   # dplyr::slice(1:20) %>%
   dplyr::mutate(Description = cleanHTML(Description)) %>% 
-  tidytext::unnest_tokens(bigram, Description, token="ngrams", n=2)
+  tidytext::unnest_tokens(ngram, Description, token="ngrams", n=ng) %>% 
+  dplyr::mutate(ngram = stringr::str_extract(ngram, paste(rep("[a-z']+", ng), collapse = " "))) %>% # get only words
+  dplyr::filter(!is.na(ngram))
 
-df_bigrams %>% 
-  dplyr::count(bigram, sort = TRUE)
+df_ngrams %>% 
+  dplyr::count(ngram, sort = TRUE)
 
-df_bigrams %>%
-  tidyr::separate(bigram, c("word1", "word2"), sep = " ") %>%
-  dplyr::filter(!word1 %in% stop_words$word) %>%
-  dplyr::filter(!word2 %in% stop_words$word) %>% 
-  dplyr::count(word1, word2, sort = TRUE) %>%
-  tidyr::unite(bigram, word1, word2, sep = " ")
+df_ngrams %>%
+  tidyr::separate(ngram, paste0("word", 1:ng), sep = " ") %>% 
+  dplyr::filter_at(vars(starts_with("word")), all_vars(!(. %in% stop_words$word))) %>% 
+  dplyr::group_by_at(vars(starts_with("word"))) %>% 
+  dplyr::summarise(n=n()) %>% 
+  tidyr::unite(ngram, paste0("word", 1:ng), sep = " ")
 
-df_bigrams %>%
-  tidyr::separate(bigram, c("word1", "word2"), sep = " ") %>%
-  dplyr::filter(!word1 %in% stop_words$word) %>%
-  dplyr::filter(!word2 %in% stop_words$word) %>% 
-  dplyr::count(Job_Type, word1, word2, sort = TRUE) %>%
-  tidyr::unite(bigram, word1, word2, sep = " ") %>%
-  # tidytext::bind_tf_idf(bigram, Job_Type, n) %>%
+df_ngrams %>%
+  tidyr::separate(ngram, paste0("word", 1:ng), sep = " ") %>% 
+  dplyr::filter_at(vars(starts_with("word")), all_vars(!(. %in% stop_words$word))) %>% 
+  dplyr::group_by_at(vars(Job_Type, starts_with("word"))) %>% 
+  dplyr::summarise(n=n()) %>% 
+  tidyr::unite(ngram, paste0("word", 1:ng), sep = " ") %>%
+  tidytext::bind_tf_idf(ngram, Job_Type, n) %>%
   dplyr::group_by(Job_Type) %>% 
-  dplyr::top_n(10) %>%
+  dplyr::top_n(15, n) %>%
   dplyr::ungroup() %>%
-  dplyr::arrange(Job_Type, n) %>%
-  dplyr::mutate(bigram = reorder(bigram, n)) %>%
-  ggplot2::ggplot(ggplot2::aes(bigram, n, order=n, fill = Job_Type)) +
-  ggplot2::geom_col(show.legend = FALSE) +
-  # ggplot2::labs(x = NULL, y = "tf-idf") +
+  ggplot2::ggplot() +
+  ggplot2::geom_col(ggplot2::aes(ngram, n, fill = Job_Type), show.legend = FALSE) +
   ggplot2::facet_wrap(~Job_Type, nrow = 1, scales = "free") +
   ggplot2::coord_flip()
+
+
+
+# Network Representation --------------------------------------------------
+
+set.seed(100)
+ar <- grid::arrow(type = "closed", length = grid::unit(.15, "inches"))
+df_ngrams %>%
+  tidyr::separate(ngram, paste0("word", 1:ng), sep = " ") %>% 
+  dplyr::filter_at(vars(starts_with("word")), all_vars(!(. %in% stop_words$word))) %>% 
+  dplyr::group_by_at(vars(starts_with("word"))) %>% 
+  dplyr::summarise(n=n()) %>%
+  dplyr::filter(n > 400) %>%
+  # dplyr::top_n(10, -n) %>% 
+  igraph::graph_from_data_frame() %>% 
+  ggraph::ggraph(layout = "fr") +
+  ggraph::geom_edge_link(ggplot2::aes(edge_alpha = scales::rescale(n, c(0.15, 1))), 
+                         show.legend = FALSE, arrow = ar, end_cap = ggraph::circle(.07, 'inches')) +
+  ggraph::geom_node_point(color = "lightblue", size = 3) +
+  ggraph::geom_node_text(ggplot2::aes(label = name), vjust = 1, hjust = 1)+
+  ggplot2::theme_void()
+
+
+# Pairs of words with the widyr package -----------------------------------
+
+df_words %>% 
+  widyr::pairwise_count(word, id, sort = TRUE) %>% head
+# Pairwise correlation: how often they appear together relative to how often they appear separately
+#  phi coefficient: how much more likely it is that either both word X and Y appear, or neither do, than that one appears without the other.
+
+word_cors <- df_words %>%
+  dplyr::group_by(word) %>%
+  dplyr::filter(dplyr::n() >= 1000) %>%
+  widyr::pairwise_cor(word, id, sort = TRUE)
+
+word_cors %>%
+  dplyr::filter(item1 %in% c("data", "machine", "business","engineer","analyst","scientist")) %>%
+  dplyr::group_by(item1) %>%
+  dplyr::top_n(10) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(item2 = reorder(item2, correlation)) %>%
+  ggplot2::ggplot(ggplot2::aes(item2, correlation)) +
+  ggplot2::geom_bar(stat = "identity") +
+  ggplot2::facet_wrap(~ item1, scales = "free_y") +
+  ggplot2::coord_flip()
+
+set.seed(2016)
+ar <- grid::arrow(type = "closed", length = grid::unit(.15, "inches"))
+word_cors %>%
+  dplyr::filter(item1 %in% c("data", "machine", "business","engineer","analyst","scientist")) %>% 
+  dplyr::filter(correlation > 0.15) %>%
+  igraph::graph_from_data_frame() %>%
+  ggraph::ggraph(layout = "fr") +
+  ggraph::geom_edge_link(ggplot2::aes(edge_alpha = scales::rescale(correlation, c(0.15, 1))), 
+                         show.legend = FALSE, arrow = ar, end_cap = ggraph::circle(.07, 'inches')) +
+  ggraph::geom_node_point(color = "lightblue", size = 3) +
+  ggraph::geom_node_text(ggplot2::aes(label = name), repel = TRUE) +
+  ggplot2::theme_void()+
+  ggplot2::theme(text = ggplot2::element_text(family = "Roboto Condensed"))
+
+
 
